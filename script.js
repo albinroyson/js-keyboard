db9bb3bb08f3a6b47afaf2d316e1f68bc42f0081
albinroyson <albinroyson5@gmail.com>
2021-11-29 16:29:10 +0530

    frist-commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("keypress",(Event) =>{
+    
+    Event.key =="e" || Event.key =="q" || Event.key =="u" || Event.key =="a" || Event.key =="l" ? console.log("equal") : console.log("notequal");
+});
